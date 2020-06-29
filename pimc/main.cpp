ed115ff59640bd12c084f852165efef3ecc0c96a
Emanuel Casiano-Diaz <Emanuel.Casiano-Diaz@uvm.edu>
2020-06-29 13:56:27 -0400

    Implemented random_boson_config

--- pimc/main.cpp
+++ pimc/main.cpp
@@ -9,11 +9,16 @@
 // A complete working C++ program to
 // demonstrate all insertion methods
 #include<iostream>
-#include<list>
 #include<vector>
-#include <boost/random.hpp>
+#include<boost/random.hpp>
+#include<cmath>
+
 using namespace std;
-  
+
+
+// Set the random number generator
+boost::random::mt19937 rng;
+
 class Kink
 {
     public:
@@ -28,30 +33,6 @@ class Kink
     friend ostream& operator<<(ostream& os, const Kink& dt);
 };
 
-class Worldlines
-{
-    public:
-    int worm_head_idx,worm_tail_idx;
-    int num_kinks;
-    
-    // Member function declarations
-    Worldlines (int,int,int); // constructor
-//    bool insert_worm(),delete_worm();
-//    bool insert_anti(),delete_anti();
-//    bool advance_head(),recede_head();
-//    bool advance_tail(),recede_tail();
-//    bool insertZero_worm(),deleteZero_worm();
-//    bool insertBeta_worm(),deleteBeta_worm();
-//    bool insert_kink_before_head(),delete_kink_before_head();
-//    bool insert_kink_after_head(),delete_kink_after_head();
-//    bool insert_kink_before_tail(),delete_kink_before_tail();
-//    bool insert_kink_after_tail(),delete_kink_after_tail();
-    
-    // Make "<<" a friend of the Worldline class
-    friend ostream& operator<<(ostream& os, const Worldlines& dt);
-    
-};
-
 // Member function definitions
 Kink::Kink (float a,int b,int c,int d,int e,int f){
     tau = a;
@@ -60,22 +41,6 @@ Kink::Kink (float a,int b,int c,int d,int e,int f){
     dir = d;
     prev = e;
     next = f;
-}
-
-Worldlines::Worldlines (int L,int D,int N){
-    Kink kink (-1,-1,-1,-1,-1,-1);
-    vector<Kink> kinks_array (100,kink);
-
-    for (int i=0; i<L; i++){
-        
-        // Modify kink attributes
-        kinks_array[i].tau = 0;
-        kinks_array[i].n = 1;
-        kinks_array[i].site = i;
-        kinks_array[i].dir = 0;
-        kinks_array[i].prev = -1;
-        kinks_array[i].next = -1;
-        }
 }
 
 // Overload "<<" operator
@@ -87,41 +52,77 @@ ostream& operator<<(ostream& os, const Kink& dt)
     return os;
 }
 
+// Function definitions
+vector<unsigned int> random_boson_config(int M,int N){
+    // Generates random Fock state of N bosons in M=L^D sites
+    
+    vector<unsigned int> alpha (M,0);
+    int site;
+    
+    // Initialize the distribution object. Note: Support is fully closed [0,M-1]
+    boost::random::uniform_int_distribution<> sites(0, M-1);
+    
+    // Randomly sprinkle the N particles among sites
+    for (int n=1; n<N; n++){
+        site = sites(rng);
+        alpha[site] += 1;
+    }
+    return alpha;
+}
+
 // Main
 int main(){
     
-    int L = 4, N = 4;
-    int num_kinks = L;
+    // Create an uniform distribution with support: [0.0,1.0)
+    boost::random::uniform_real_distribution<double> rnum(0.0, 1.0);
     
+    // Bose-Hubbard parameters
+    int L = 4, D = 1, N = 4;
+    float t = 1.0, U = 1.0, mu = 0.5;
+    vector<unsigned int> alpha;
+    int M = pow(L,D); // total sites
+    
+    // Simulation parameters
+    float eta = 1.0, beta = 1.0;
+    
+    // Counters and trackers
+    int num_kinks = M;
+    int worm_head_idx = -1, worm_tail_idx = -1;
+
+    // Initialize Kink object
     Kink kink (-1,-1,-1,-1,-1,-1);
     
-    vector<Kink> kinks_array (100,kink);
+    // Pre-allocate vector that will store all kinks in the worldline
+    vector<Kink> kinks_vector (100,kink);
                 
-    for (int i=0; i<100; i++){
-        
-        if (i<L){
+    // Initialize the first L^D kinks
+    for (int i=0; i<L; i++){
         // Modify kink attributes
-        kinks_array[i].tau = 0;
-        kinks_array[i].n = 1;
-        kinks_array[i].site = i;
-        kinks_array[i].dir = 0;
-        kinks_array[i].prev = -1;
-        kinks_array[i].next = -1;
-        }
-        
-        // print out the initialized kinks
-        cout << kinks_array[i] << endl;
-        
+        kinks_vector[i].tau = 0;
+        kinks_vector[i].n = 1;
+        kinks_vector[i].site = i;
+        kinks_vector[i].dir = 0;
+        kinks_vector[i].prev = -1;
+        kinks_vector[i].next = -1;
     }
     
-    cout << "num_kinks: " << num_kinks << endl;
+    // Total number of lattice points
+    cout << "total_sites: " << M << endl;
     
-    // Test random number generation with Boost
-    boost::random::mt19937 rng;
-    boost::random::uniform_real_distribution<double> rnum(0.0, 1.0);
+    // Print out the number of kinks
+    cout << "num_kinks: " << num_kinks << endl;
+
+    // Generate some random numbers to test the generator
     for (int i = 0; i < 10; ++i) {
         std::cout << rnum(rng) << "\n";
     }
+    
+    // Generate a random fock state
+    alpha = random_boson_config(M,N);
+    for (int i=0;i<M;i++){
+        cout << alpha[i];
+    }
+    cout << endl;
     
     return 0;
 }
