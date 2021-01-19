5ed2b7c7a7f61c0e9e0ff94e7f8301c34ac3bf0f
Emanuel Casiano-Diaz <ecasiano@acf-login7.acf.nics.utk.edu>
2021-01-19 06:47:11 -0500

    Generated beta scaling data 1,2,3. Entanglement combiner is more detailed now

--- pimc/Scripts/entanglement_combiner.py
+++ pimc/Scripts/entanglement_combiner.py
@@ -5,22 +5,49 @@ import os
 import numpy as np
 
 # Save all the file names in the path as strings to a list
-path = '../Data/'
+path = '../'
 filenames_all = os.listdir(path)
 
-# Only keep the files that contain the string 'SWAP' in their name,
-# the desired "U" in their name, and that were able to collect all bins
-string_to_keep = '_SWAP_'  # The string that kept files will contain
-U_to_keep = '3.300000'
-bins_wanted = 1000
+# Saves the files relevant to the Renyi Entanglement Entropy calculation
 files_SWAP = []
+
+# Iterate over all filenames
 for filename in filenames_all:
-    if (string_to_keep in filename) and (U_to_keep in filename):
-        if os.stat(path+filename).st_size > 0:
-            with open(path+filename) as f:
-               count = sum(1 for _ in f)
-            if count == bins_wanted:
-                files_SWAP.append(filename)
+    
+    # Extract parameter information from file name
+    parameters = filename.split("_")
+    
+    if parameters[0]=='1D' or parameters[0]=='2D' or parameters[0]=='3D':
+    
+        D = int((parameters[0])[0]) # hypercube dimension
+        L = int(parameters[1]) # hypercube linear size
+        N = int(parameters[2]) # total particles
+        l = int(parameters[3]) # subsystem linear size
+        U = float(parameters[4]) # interaction potential
+        t = float(parameters[5]) # tunneling parameter
+        beta = float(parameters[6]) # imaginary time length (K_B*T)**(-1)
+        bins_wanted = int(parameters[7]) # number of bins saved in file
+        filetype = parameters[8] # identifies the data stored in file
+    #     seed = parameters[9].split(".")[0] # random seed used
+
+        if filetype=='SWAP':
+            # Set parameters of simulations from differenet seeds we want to evaluate [D,L,N,l,U,t,beta,bins,type]
+            parameters_to_evaluate = [1,
+                                      4,
+                                      4,
+                                      2,
+                                      4.000000,
+                                      1.000000,
+                                      3.000000,
+                                      1000,
+                                      'SWAP']
+
+            if [D,L,N,l,U,t,beta,bins_wanted,filetype] == parameters_to_evaluate:
+                if os.stat(path+filename).st_size > 0:
+                    with open(path+filename) as f:
+                       count = sum(1 for _ in f)
+                    if count == bins_wanted: # only consider files that managed to save number of bins wanted
+                        files_SWAP.append(filename)
         
 # Sort SWAP files in ascending order of random seed
 files_SWAP.sort()
@@ -46,6 +73,8 @@ S2_stderr = np.std(S2_data,axis=0)/np.sqrt(number_of_seeds)
 
 # Print out <S2> +/- error
 for l in range(columns_per_file):
-    print(f"<S2(l={l})> = {S2_mean[l]} +/- {S2_stderr[l]:0.8f}")
+    print(f"<S2(l={l})> = {S2_mean[l]:0.8f} +/- {S2_stderr[l]:0.8f}")
     
 print(number_of_seeds)
+
+
