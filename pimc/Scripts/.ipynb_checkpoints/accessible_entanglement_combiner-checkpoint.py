95f83513b0a8f713eaf8b72d95f5ff104ae40f30
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-02-19 16:27:34 -0500

    Added some slurm scripts

--- pimc/Scripts/.ipynb_checkpoints/accessible_entanglement_combiner-checkpoint.py
+++ pimc/Scripts/.ipynb_checkpoints/accessible_entanglement_combiner-checkpoint.py
@@ -19,21 +19,30 @@ seeds_list = list(range(1000))
 seeds_measured = []
 
 # Save all the file names in the path as strings to a list
-path = path="/Users/ecasiano/Desktop/PrototypeScripts/TestSystematicError/"
+path = path="/Users/ecasiano/Desktop/PrototypeScripts/PhaseDiagramN100/"
+path = path="/Users/ecasiano/Desktop/PrototypeScripts/TestSystematicErrorv5/"
+# path = path="/Users/ecasiano/Desktop/PrototypeScripts/TestAccuracy100binsN4/"
+
 filenames_all = os.listdir(path)
 
 # Set desired total number of particles
-L_want = 4
-N_want = 4
-l_want = 2
+L_want = 10
+N_want = 10
+l_want = 5
 beta_want = 4.000000
-bin_size_want = 10000
+bin_size_want = 1000
 bins_want = 1000
 D_want = 1
 U_want = 3.300000
 t_want = 1.000000
 
-for mA_sector_wanted in range(1,l_want**D_want+1):
+mA_sector_wanted = l_want # 1D
+
+U_list = np.round(np.geomspace(0.01,100,20),4)
+U_list = np.array([0.01,3.3])
+
+# for mA_sector_wanted in range(l_want**D_want,l_want**D_want+1):
+for U_want in U_list:
 
     # Saves the files relevant to P(n) & S2(n) calculation
     files_PnSquared = []
@@ -102,7 +111,7 @@ for mA_sector_wanted in range(1,l_want**D_want+1):
     print('Initial number of seeds: ',number_of_seeds,"\n")
     PnSquared_col_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
     Pn_col_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
-    SWAP_col_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
+    SWAPncol_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
     for s in range(number_of_seeds):
 
         data = np.loadtxt(path+files_PnSquared[s])
@@ -112,55 +121,61 @@ for mA_sector_wanted in range(1,l_want**D_want+1):
         Pn_col_sums[s] = np.sum(data,axis=0)
 
         data = np.loadtxt(path+files_SWAPn[s])
-        SWAP_col_sums[s] = np.sum(data,axis=0)
-        
+        SWAPncol_sums[s] = np.sum(data,axis=0)
+
     # Jacknife 
-    SWAP_col_seed_sum = np.sum(SWAP_col_sums,axis=0)
+    SWAPncol_seed_sum = np.sum(SWAPncol_sums,axis=0)
     PnSquared_col_seed_sum = np.sum(PnSquared_col_sums,axis=0)
-    Pn_col_seed_sum = np.sum(Pn_col_sums,axis=0)
-    S2_i = np.zeros(SWAP_col_sums.shape)
-    S2_acc_i = np.zeros(SWAP_col_sums.shape[0])
-    for i in range(SWAP_col_sums.shape[0]):
-        
-        SWAP_jacknifed_sum = SWAP_col_seed_sum - SWAP_col_sums[i]
+    Pn_col_seed_sum = np.sum(Pn_col_sums,axis=0) # dim: [number of seeds \times N+1]
+
+    S2n_jacknifed = np.zeros(SWAPncol_sums.shape)
+    S2acc_jacknifed = np.zeros(SWAPncol_sums.shape[0])
+    Pn_jacknifed = np.zeros(Pn_col_sums.shape)
+    for i in range(SWAPncol_sums.shape[0]):
+
+        SWAPnjacknifed_sum = SWAPncol_seed_sum - SWAPncol_sums[i]
         PnSquared_jacknifed_sum = PnSquared_col_seed_sum - PnSquared_col_sums[i]
         Pn_jacknifed_sum = Pn_col_seed_sum - Pn_col_sums[i]
 
-        S2_i[i] = -np.log ( SWAP_jacknifed_sum / PnSquared_jacknifed_sum ) 
-        S2_acc_i[i] = np.sum(Pn_jacknifed_sum * S2_i[i]) / np.sum(Pn_jacknifed_sum)
+        # n-resolved Renyi Entropy for data poin i
+        S2n_jacknifed[i] = -np.log ( SWAPnjacknifed_sum / PnSquared_jacknifed_sum ) 
 
-    S2 = S2_i
-    
-    # Get Pn
-    Pn = Pn_col_sums / np.sum(Pn_col_sums,axis=1)[:,None]
+        # Accessible Renyi Entropy for data poin i (ASK ADRIAN & CHRIS about correctness of nan_to_num)
+    #     S2acc_jacknifed[i] = np.sum(Pn_jacknifed_sum * S2n_jacknifed[i]) / np.sum(Pn_jacknifed_sum)
+        S2acc_jacknifed[i] = np.sum(Pn_jacknifed_sum * np.nan_to_num(S2n_jacknifed[i],posinf=0)) / np.sum(Pn_jacknifed_sum)
 
-    # Some seeds might have been thrown away
-#     number_of_seeds = SWAP.shape[0]
+
+        # Local particle number distribution for data poin i
+        Pn_jacknifed[i] = Pn_jacknifed_sum / np.sum(Pn_jacknifed_sum)
 
     print("\nFinal number of seeds: ",number_of_seeds)
 
-    S2_mean = np.mean(S2,axis=0)
-    S2_err = np.std(S2,axis=0) * np.sqrt(S2.shape[0])
+    # Calculate Renyi Entropies of each local particle number sector
+    S2n_mean = np.mean(S2n_jacknifed,axis=0)
+    S2n_err = np.std(S2n_jacknifed,axis=0) * np.sqrt(S2n_jacknifed.shape[0])
 
-    S2_acc_mean = np.mean(S2_acc_i,axis=0)
-    S2_acc_err = np.std(S2_acc_i,axis=0) * np.sqrt(S2_acc_i.shape[0])
+    # Replace nan due to log(0) or 0/0 with zeros (ASK ADRIAN & CHRIS)
+    # S2n_mean = np.nan_to_num(S2n_mean)
+    # S2n_err = np.nan_to_num(S2n_err)
+
+    # Calculate the total accessible entanglement entropy
+    S2acc_mean = np.mean(S2acc_jacknifed,axis=0)
+    S2acc_err = np.std(S2acc_jacknifed,axis=0) * np.sqrt(S2acc_jacknifed.shape[0])
 
     # Get mean of Pn and std errors
-    Pn_mean = np.mean(Pn,axis=0)
-    Pn_err = np.std(Pn,axis=0) / np.sqrt(number_of_seeds)
+    Pn_mean = np.mean(Pn_jacknifed,axis=0)
+    Pn_err = np.std(Pn_jacknifed,axis=0) * np.sqrt(number_of_seeds)
 
     print("\npartition size: ", mA_sector_wanted,"\n")
     # Print P(n),S2(n) for each sector to screen
-    for i in range(len(S2_mean)):
+    for i in range(len(S2n_mean)):
         print("P(n=%d) = %.4f +/- %.4f"%(i,Pn_mean[i],Pn_err[i]))
     print("\n")
-    for i in range(len(S2_mean)):
-        print("S2(n=%d) = %.4f +/- %.4f"%(i,S2_mean[i],S2_err[i]))
-        
-    print("\nS2_acc = %.4f +/- %.4f"%(S2_acc_mean,S2_acc_err))
-    
-print("\nincomplete seeds: ",[int(i) for i in incomplete_seeds])
+    for i in range(len(S2n_mean)):
+        print("S2(n=%d) = %.4f +/- %.4f"%(i,S2n_mean[i],S2n_err[i]))
 
+    print("\nS2acc = %.4f +/- %.4f"%(S2acc_mean,S2acc_err))
+    
 # print("Seeds not included for some reason: ")
 
 # for i in seeds_list:
