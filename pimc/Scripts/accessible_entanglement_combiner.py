c8a40fea697a34929be13ba6c3e768c82b2601a7
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-04-10 12:24:55 -0400

    Energies will be printed out now even if SWAP measurements are being done

--- pimc/Scripts/accessible_entanglement_combiner.py
+++ pimc/Scripts/accessible_entanglement_combiner.py
@@ -14,170 +14,235 @@
 import os
 import numpy as np
 
-incomplete_seeds = [] 
-seeds_list = list(range(1000))
-seeds_measured = []
+# Set values of U sweep
+# U_list = np.round(np.geomspace(0.01,100,20),4)
+U_list = np.array([3.3])
+# U_list = np.array([0.500000,
+# 0.730000,
+# 1.065800,
+# 1.556100, 
+# 2.272000, 
+# 3.300000, 
+# 4.843100, 
+# 7.071100,   
+# 10.323900,
+# 16.666667, 
+# 22.007100, 
+# 32.130800,
+# 46.911700,
+# 68.492100,
+# 100.000000])
 
-# Save all the file names in the path as strings to a list
-path = path="/Users/ecasiano/Desktop/PrototypeScripts/PhaseDiagramN100/"
-path = path="/Users/ecasiano/Desktop/PrototypeScripts/TestSystematicErrorv5/"
-# path = path="/Users/ecasiano/Desktop/PrototypeScripts/TestAccuracy100binsN4/"
+beta_list = [0.6,0.7,0.8,0.9,1.0,1.15,1.30,1.50,
+             1.75,2.0,2.25,2.50,2.75,3.0,3.25,
+             3.50,3.75,4.0,5.0,6.0]
+# beta_list = [0.6,0.7,0.8,0.9,1.0,1.15,1.30,1.50,
+#              1.75,2.0,2.5,3.0,3.5,
+#              4.0,4.5,5.0,5.5,6.0]
+beta_list = [0.6,0.7,0.8,0.9,1.0,1.15,1.30,1.50,
+             1.75,2.0,3.0,4.0]
+beta_list = [7.0]
 
-filenames_all = os.listdir(path)
+S2_plot = []
+S2_err_plot = []
+for U in U_list:
+    for beta in beta_list:
+        for l_sector_wanted in [8]:
+        
+            incomplete_seeds = [] 
+            seeds_list = list(range(1000))
+            seeds_measured = []
 
-# Set desired total number of particles
-L_want = 10
-N_want = 10
-l_want = 5
-beta_want = 4.000000
-bin_size_want = 1000
-bins_want = 1000
-D_want = 1
-U_want = 3.300000
-t_want = 1.000000
+            # Set desired parameters of calculation
+            L = "%d"%(2*l_sector_wanted)
+            N = "%d"%(2*l_sector_wanted)
+            l_max = "%d"%l_sector_wanted
+            beta = "%.6f"%beta
+            bin_size = "1000"
+            bins_wanted = "1000"
+            D = "1"
+            U = "%.6f"%(U)
+            t = "1.000000"
 
-mA_sector_wanted = l_want # 1D
+            # Get path where raw data for the simulation is stored
+            path = D+"D_"+L+"_"+N+"_"+l_max+"_"+U+"_"+\
+                   t+"_"+beta+"_"+bin_size+"_"+bins_wanted+"/"
+            path = "/Users/ecasiano/Desktop/Data/"+path
 
-U_list = np.round(np.geomspace(0.01,100,20),4)
-U_list = np.array([0.01,3.3])
+            # Stores all files in the directory
+            filenames_all = os.listdir(path)
 
-# for mA_sector_wanted in range(l_want**D_want,l_want**D_want+1):
-for U_want in U_list:
+            # Set size of subregion A we want to calculate (mA=l^D)
+    #         l_sector_wanted = int(L)//2
+    #         l_sector_wanted = 4
 
-    # Saves the files relevant to P(n) & S2(n) calculation
-    files_PnSquared = []
-    files_SWAPn = []
-    files_Pn = []
+            # Convert strings back to numbers
+            L = int(L)
+            N = int(N)
+            l_max = int(l_max)
+            beta = float(beta)
+            bin_size = int(bin_size)
+            bins_wanted = int(bins_wanted)
+            D = int(D)
+            U = float(U)
+            t = float(t)
 
-    # Iterate over all filenames in path
-    for filename in filenames_all:
+            # Saves the files relevant to P(n) & S2(n) calculation
+            files_PnSquared = []
+            files_SWAPn = []
+            files_Pn = []
 
-        # Extract parameter information from file name
-        parameters = filename.split("_")
+            # Iterate over all filenames in path
+            for filename in filenames_all:
 
-        if parameters[0]=='1D' or parameters[0]=='2D' or parameters[0]=='3D':
+                # Extract parameter information from file name
+                parameters = filename.split("_")
 
-            D = int((parameters[0])[0]) # hypercube dimension
-            L = int(parameters[1]) # hypercube linear size
-            N = int(parameters[2]) # total particles
-            l = int(parameters[3]) # subsystem linear size (actually l_max)
-            U = float(parameters[4]) # interaction potential
-            t = float(parameters[5]) # tunneling parameter
-            beta = float(parameters[6]) # imaginary time length (K_B*T)**(-1)
-            bin_size = int(parameters[7])
-            bins_wanted = int(parameters[8]) # number of bins saved in file
-            filetype = (parameters[9]).split("-mA") # identifies the data stored in file
-            seed = int(parameters[10].split(".")[0]) # random seed used
+                if parameters[0]=='1D' or parameters[0]=='2D' or parameters[0]=='3D':
 
-            if filetype[0]=='PnSquared' and int(filetype[1])==mA_sector_wanted:
-                # Set parameters of simulations from differenet seeds we want to evaluate [D,L,N,l,U,t,beta,bins,type]
-                parameters_to_evaluate = [D_want,
-                                          L_want,
-                                          N_want,
-                                          l_want,
-                                          U_want,
-                                          t_want,
-                                          beta_want,
-                                          bin_size_want,
-                                          bins_want,
-                                          'PnSquared']
+                    D_want = int((parameters[0])[0]) # hypercube dimension
+                    L_want = int(parameters[1]) # hypercube linear size
+                    N_want = int(parameters[2]) # total particles
+                    l_want = int(parameters[3]) # subsystem linear size (actually l_max)
+                    U_want = float(parameters[4]) # interaction potential
+                    t_want = float(parameters[5]) # tunneling parameter
+                    beta_want = float(parameters[6]) # imaginary time length (K_B*T)**(-1)
+                    bin_size_want = int(parameters[7])
+                    bins_want = int(parameters[8]) # number of bins saved in file
+                    filetype = (parameters[9]).split("-mA") # identifies the data stored in file
+                    seed = int(parameters[10].split(".")[0]) # random seed used
 
-                if [D,L,N,l,U,t,beta,bin_size,bins_wanted,filetype[0]] == parameters_to_evaluate:
-                    if os.stat(path+filename).st_size > 0:
-                        with open(path+filename) as f:
-                           count = sum(1 for _ in f)
-                        if count == bins_wanted: # only consider files that managed to save number of bins wanted
-                            files_PnSquared.append(filename)
+                    if filetype[0]=='PnSquared' and int(filetype[1])==l_sector_wanted:
+                        # Set parameters of simulations from different seeds we want to evaluate [D,L,N,l,U,t,beta,bins,type]
+                        parameters_to_evaluate = [D_want,
+                                                  L_want,
+                                                  N_want,
+                                                  l_want,
+                                                  U_want,
+                                                  t_want,
+                                                  beta_want,
+                                                  bin_size_want,
+                                                  bins_want,
+                                                  'PnSquared']
 
-                            filename_splitted = filename.split('_')
-                            filename_splitted[9] = 'Pn-mA'+str(mA_sector_wanted)
-                            filename_Pn = "_".join(filename_splitted)
-                            files_Pn.append(filename_Pn)
+                        if [D,L,N,l_max,U,t,beta,bin_size,bins_wanted,filetype[0]] == parameters_to_evaluate:
+                            if os.stat(path+filename).st_size > 0:
+                                with open(path+filename) as f:
+                                   count = sum(1 for _ in f)
+                                if count == bins_wanted: # only consider files that managed to save number of bins wanted
+                                    files_PnSquared.append(filename)
 
-                            filename_splitted = filename.split('_')
-                            filename_splitted[9] = 'SWAPn-mA'+str(mA_sector_wanted)
-                            filename_SWAPn = "_".join(filename_splitted)
-                            files_SWAPn.append(filename_SWAPn)
+                                    filename_splitted = filename.split('_')
+                                    filename_splitted[9] = 'Pn-mA'+str(l_sector_wanted)
+                                    filename_Pn = "_".join(filename_splitted)
+                                    files_Pn.append(filename_Pn)
 
-                            seeds_measured.append(seed)
+                                    filename_splitted = filename.split('_')
+                                    filename_splitted[9] = 'SWAPn-mA'+str(l_sector_wanted)
+                                    filename_SWAPn = "_".join(filename_splitted)
+                                    files_SWAPn.append(filename_SWAPn)
 
-                        else: 
-                            incomplete_seeds.append(seed)
+                                    seeds_measured.append(seed)
 
-    # Get total number of seeds 
-    number_of_seeds = len(files_PnSquared)//1
+                                else: 
+                                    incomplete_seeds.append(seed)
 
-    # Get column sum of P(n) files for each seed
-    print('Initial number of seeds: ',number_of_seeds,"\n")
-    PnSquared_col_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
-    Pn_col_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
-    SWAPncol_sums = np.zeros((number_of_seeds,N_want+1)).astype(int)
-    for s in range(number_of_seeds):
+            # Get total number of seeds 
+            number_of_seeds = len(files_PnSquared)//1
 
-        data = np.loadtxt(path+files_PnSquared[s])
-        PnSquared_col_sums[s] = np.sum(data,axis=0)
+            print("\nN: ",N)
+            print("L: ",L)
+            print("partition size: ", l_sector_wanted)
+            print("U: ",U)
+            print("beta: ",beta)
 
-        data = np.loadtxt(path+files_Pn[s])
-        Pn_col_sums[s] = np.sum(data,axis=0)
+            # Get column sum of P(n) files for each seed
+            print('\nInitial number of seeds: ',number_of_seeds)
+            PnSquared_col_sums = np.zeros((number_of_seeds,N+1)).astype(int)
+            Pn_col_sums = np.zeros((number_of_seeds,N+1)).astype(int)
+            SWAPn_col_sums = np.zeros((number_of_seeds,N+1)).astype(int)
+            for s in range(number_of_seeds):
 
-        data = np.loadtxt(path+files_SWAPn[s])
-        SWAPncol_sums[s] = np.sum(data,axis=0)
+                data = np.loadtxt(path+files_PnSquared[s])
+                PnSquared_col_sums[s] = np.sum(data,axis=0)
 
-    # Jacknife 
-    SWAPncol_seed_sum = np.sum(SWAPncol_sums,axis=0)
-    PnSquared_col_seed_sum = np.sum(PnSquared_col_sums,axis=0)
-    Pn_col_seed_sum = np.sum(Pn_col_sums,axis=0) # dim: [number of seeds \times N+1]
+                data = np.loadtxt(path+files_Pn[s])
+                Pn_col_sums[s] = np.sum(data,axis=0)
 
-    S2n_jacknifed = np.zeros(SWAPncol_sums.shape)
-    S2acc_jacknifed = np.zeros(SWAPncol_sums.shape[0])
-    Pn_jacknifed = np.zeros(Pn_col_sums.shape)
-    for i in range(SWAPncol_sums.shape[0]):
+                data = np.loadtxt(path+files_SWAPn[s])
+                SWAPn_col_sums[s] = np.sum(data,axis=0)
 
-        SWAPnjacknifed_sum = SWAPncol_seed_sum - SWAPncol_sums[i]
-        PnSquared_jacknifed_sum = PnSquared_col_seed_sum - PnSquared_col_sums[i]
-        Pn_jacknifed_sum = Pn_col_seed_sum - Pn_col_sums[i]
+            # Jacknife 
+            SWAPn_col_seed_sum = np.sum(SWAPn_col_sums,axis=0) # shape: (1,N+1)... I think
+            PnSquared_col_seed_sum = np.sum(PnSquared_col_sums,axis=0)
+            Pn_col_seed_sum = np.sum(Pn_col_sums,axis=0)
 
-        # n-resolved Renyi Entropy for data poin i
-        S2n_jacknifed[i] = -np.log ( SWAPnjacknifed_sum / PnSquared_jacknifed_sum ) 
+            S2n_jacknifed = np.zeros(SWAPn_col_sums.shape)
+            S2acc_jacknifed = np.zeros(SWAPn_col_sums.shape[0]) # one element for each seed
+            Pn_jacknifed = np.zeros(Pn_col_sums.shape)
+            ratio_jacknifed = np.zeros(SWAPn_col_sums.shape)
+            for i in range(SWAPn_col_sums.shape[0]):
 
-        # Accessible Renyi Entropy for data poin i (ASK ADRIAN & CHRIS about correctness of nan_to_num)
-    #     S2acc_jacknifed[i] = np.sum(Pn_jacknifed_sum * S2n_jacknifed[i]) / np.sum(Pn_jacknifed_sum)
-        S2acc_jacknifed[i] = np.sum(Pn_jacknifed_sum * np.nan_to_num(S2n_jacknifed[i],posinf=0)) / np.sum(Pn_jacknifed_sum)
+                # Generate jacknifed data
+                SWAPn_jacknifed_sum = SWAPn_col_seed_sum - SWAPn_col_sums[i]
+                PnSquared_jacknifed_sum = PnSquared_col_seed_sum - PnSquared_col_sums[i]
+                Pn_jacknifed_sum = Pn_col_seed_sum - Pn_col_sums[i]
 
+                # n-resolved Renyi Entropy for data point i
+                ratio_jacknifed[i] = SWAPn_jacknifed_sum / PnSquared_jacknifed_sum
+                S2n_jacknifed[i] = -np.log ( ratio_jacknifed[i] ) 
 
-        # Local particle number distribution for data poin i
-        Pn_jacknifed[i] = Pn_jacknifed_sum / np.sum(Pn_jacknifed_sum)
+                # Grab indices of n-sectors where S2acc will not be NaN or inf.
+                good_nsectors = np.argwhere(np.logical_not(np.isnan(ratio_jacknifed[i])) & np.logical_not(np.isinf(ratio_jacknifed[i])))
 
-    print("\nFinal number of seeds: ",number_of_seeds)
+                # Accessible Renyi Entropy for data point i
+    #             S2acc_jacknifed[i] = np.sum(Pn_jacknifed_sum * S2n_jacknifed[i]) / np.sum(Pn_jacknifed_sum)
+                S2acc_jacknifed[i] = -2*np.log(np.sum(Pn_jacknifed_sum[good_nsectors] * np.sqrt(ratio_jacknifed[i][good_nsectors])) / np.sum(Pn_jacknifed_sum[good_nsectors]))
 
-    # Calculate Renyi Entropies of each local particle number sector
-    S2n_mean = np.mean(S2n_jacknifed,axis=0)
-    S2n_err = np.std(S2n_jacknifed,axis=0) * np.sqrt(S2n_jacknifed.shape[0])
+                # Local particle number distribution for data poin i
+                Pn_jacknifed[i] = Pn_jacknifed_sum / np.sum(Pn_jacknifed_sum)
 
-    # Replace nan due to log(0) or 0/0 with zeros (ASK ADRIAN & CHRIS)
-    # S2n_mean = np.nan_to_num(S2n_mean)
-    # S2n_err = np.nan_to_num(S2n_err)
+            print("Final number of seeds: ",number_of_seeds,"\n")     
 
-    # Calculate the total accessible entanglement entropy
-    S2acc_mean = np.mean(S2acc_jacknifed,axis=0)
-    S2acc_err = np.std(S2acc_jacknifed,axis=0) * np.sqrt(S2acc_jacknifed.shape[0])
+            # Calculate Renyi Entropies of each local particle number sector
+            S2n_mean = np.mean(S2n_jacknifed,axis=0)
+            S2n_err = np.std(S2n_jacknifed,axis=0) * np.sqrt(S2n_jacknifed.shape[0])
 
-    # Get mean of Pn and std errors
-    Pn_mean = np.mean(Pn_jacknifed,axis=0)
-    Pn_err = np.std(Pn_jacknifed,axis=0) * np.sqrt(number_of_seeds)
+            # Calculate the total accessible entanglement entropy. Old way. MIGHT need error propagation and sum up all sectors instead.
+            S2acc_mean = np.mean(S2acc_jacknifed,axis=0)
+            S2acc_err = np.std(S2acc_jacknifed,axis=0) * np.sqrt(S2acc_jacknifed.shape[0])
 
-    print("\npartition size: ", mA_sector_wanted,"\n")
-    # Print P(n),S2(n) for each sector to screen
-    for i in range(len(S2n_mean)):
-        print("P(n=%d) = %.4f +/- %.4f"%(i,Pn_mean[i],Pn_err[i]))
-    print("\n")
-    for i in range(len(S2n_mean)):
-        print("S2(n=%d) = %.4f +/- %.4f"%(i,S2n_mean[i],S2n_err[i]))
+            # Get mean of Pn and std errors
+            Pn_mean = np.mean(Pn_jacknifed,axis=0)
+            Pn_err = np.std(Pn_jacknifed,axis=0) * np.sqrt(number_of_seeds)
 
-    print("\nS2acc = %.4f +/- %.4f"%(S2acc_mean,S2acc_err))
-    
-# print("Seeds not included for some reason: ")
+            # Print P(n),S2(n) for each sector to screen
+            for i in range(len(S2n_mean)):
+                print("P(n=%d) = %.4f +/- %.4f"%(i,Pn_mean[i],Pn_err[i]))
+            print("\n")
+            for i in range(len(S2n_mean)):
+                print("S2(n=%d) = %.4f +/- %.4f"%(i,S2n_mean[i],S2n_err[i]))
 
-# for i in seeds_list:
-#     if not(i in seeds_measured):
-#         print(i,end=",")
+            print("\nS2acc = %.4f +/- %.4f \n"%(S2acc_mean,S2acc_err))
+            
+            S2_plot.append(S2acc_mean)
+            S2_err_plot.append(S2acc_err)
+
+            # Check that throwaway error is smaller than standard error
+            bad_nsectors = np.argwhere(np.isnan(S2acc_mean))
+            throwaway_err = np.sum(Pn_mean[bad_nsectors])
+            if (throwaway_err/S2acc_err > 0.1):
+                print("WARNING: Throwaway error larger than 10% of the standard error")
+                print("Throwaway Error: ",throwaway_err)
+                print("S2acc_err: ",S2acc_err)
+                
+
+        # print("Seeds not included for some reason: ")
+
+        # for i in seeds_list:
+        #     if not(i in seeds_measured):
+        #         print(i,end=",")
+        
+print("<S2Acc>=",S2_plot)
+print("S2Acc_err=",S2_err_plot)
+print("beta=",beta_list)
