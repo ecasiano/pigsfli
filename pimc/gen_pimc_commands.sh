f60b4b2000bf183499f80c0011c6defd1e968d01
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-04-15 13:38:31 -0400

    MOdified gen_pimc_commands.sh

--- pimc/gen_pimc_commands.sh
+++ pimc/gen_pimc_commands.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 pimc_bin="$HOME/pimc/pimc/pimc.e";
 
-pimc_args="-U 3.3 -D 1 -L 16 -N 16 -l 8 --beta 7 --bin-size 1000 --bins-wanted 1000 --mu 6 --subgeometry  square";
+pimc_args="-D 1 -L 4 -N 4 -l 2 --bin-size 10000 --bins-wanted 1000 -U 3.3 --mu 6.0 --subgeometry square --num-replicas 1 --sweeps 50000000 --measurement-frequency 25 --beta 4.0 --seed 2"
 
 command_file="pimc_commands";
 for i in {0..499}
