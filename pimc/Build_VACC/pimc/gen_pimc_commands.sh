d5df6fafe9d416b0251c297d77b37a4bf692b178
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-08-24 11:03:20 -0600

    Code can now opt to calculate only Full S2 via command line argument

--- pimc/Build_VACC/pimc/gen_pimc_commands.sh
+++ pimc/Build_VACC/pimc/gen_pimc_commands.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
 pimc_bin="/users/e/c/ecasiano/pimc/pimc/Build_VACC/pimc/pigsl.e";
 
-pimc_args="-D 1 -L 4 -N 4 -l 2 -U 3.3 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 1.0 --rng boost_mt19937 --num-replicas 2 --bin-size 1000 --bins-wanted 1000"
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 0.500000 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
 
 command_file="pimc_commands";
-for i in {0..499}
+for i in {0..164}
+do
+    cmd="${pimc_bin} ${pimc_args} --seed ${i}";
+    echo "${cmd}" >> $command_file ;
+done
+
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 0.730000 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
+
+command_file="pimc_commands";
+for i in {0..164}
+do
+    cmd="${pimc_bin} ${pimc_args} --seed ${i}";
+    echo "${cmd}" >> $command_file ;
+done
+
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 1.065800 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
+
+command_file="pimc_commands";
+for i in {0..164}
+do
+    cmd="${pimc_bin} ${pimc_args} --seed ${i}";
+    echo "${cmd}" >> $command_file ;
+done
+
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 1.556100 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
+
+command_file="pimc_commands";
+for i in {0..164}
+do
+    cmd="${pimc_bin} ${pimc_args} --seed ${i}";
+    echo "${cmd}" >> $command_file ;
+done
+
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 2.272000 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
+
+command_file="pimc_commands";
+for i in {0..164}
+do
+    cmd="${pimc_bin} ${pimc_args} --seed ${i}";
+    echo "${cmd}" >> $command_file ;
+done
+
+pimc_args="-D 1 -L 16 -N 16 -l 8 -U 3.300000 --mu 6.0 --subgeometry square --measurement-frequency 1 --beta 6.0 --rng boost_mt19937 --num-replicas 2 --bin-size 10000 --bins-wanted 100"
+
+command_file="pimc_commands";
+for i in {0..164}
 do
     cmd="${pimc_bin} ${pimc_args} --seed ${i}";
     echo "${cmd}" >> $command_file ;
