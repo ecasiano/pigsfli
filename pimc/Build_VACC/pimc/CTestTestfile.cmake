c9beceb8a9fa487749508ac3c04e1cfd1d547f5b
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-11-17 20:13:24 -0700

    Deleted everything

--- pimc/Build_VACC/pimc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/e/c/ecasiano/pimc/pimc/src/pimc
-# Build directory: /users/e/c/ecasiano/pimc/pimc/Build_VACC/pimc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
