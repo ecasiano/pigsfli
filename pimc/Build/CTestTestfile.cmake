3a3aeec5ef32de6b7de959da992b98aa3294375b
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-11-17 20:14:12 -0700

    Deleted notebooks and files that were not needed anymore

---
+++ pimc/Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ecasiano/XCode/pimc/pimc/src
+# Build directory: /Users/ecasiano/XCode/pimc/pimc/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("pimc")
