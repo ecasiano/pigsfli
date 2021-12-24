3b405e80ec347ddf0759aadd44e09da857479450
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-12-23 22:29:00 -0500

    Removed build directory

--- Build/pimc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ecasiano/Desktop/pimc/src/pimc
-# Build directory: /Users/ecasiano/Desktop/pimc/build/pimc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
