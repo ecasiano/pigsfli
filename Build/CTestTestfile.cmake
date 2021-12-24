1d0d3ebf4d78e232e5f07beb1098766bba286b3a
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2021-12-23 22:27:43 -0500

    Commented out uuid parts of the code

--- Build/CTestTestfile.cmake
+++ Build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/ecasiano/XCode/pimc/pimc/src
-# Build directory: /Users/ecasiano/XCode/pimc/pimc/Build
+# Source directory: /Users/ecasiano/Desktop/pimc/src
+# Build directory: /Users/ecasiano/Desktop/pimc/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
