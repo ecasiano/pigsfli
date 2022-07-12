a2b207f64e647500a71d4838396aefe51feea9fa
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2022-07-11 19:04:34 -0600

    this works. about to clean large .git file

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/ecasiano/Desktop/pimc/src
-# Build directory: /Users/ecasiano/Desktop/pimc/build
+# Source directory: /Users/ecasiano/Desktop/pigsfli/src
+# Build directory: /Users/ecasiano/Desktop/pigsfli/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- build/pimc/CTestTestfile.cmake
+++ build/pimc/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/ecasiano/Desktop/pimc/src/pimc
-# Build directory: /Users/ecasiano/Desktop/pimc/build/pimc
+# Source directory: /Users/ecasiano/Desktop/pigsfli/src/pimc
+# Build directory: /Users/ecasiano/Desktop/pigsfli/build/pimc
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
