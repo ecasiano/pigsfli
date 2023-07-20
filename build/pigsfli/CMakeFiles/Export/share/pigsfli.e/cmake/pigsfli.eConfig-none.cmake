f7a477fd2d6c960ec45ba432729d518cf9dbb76c
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2023-07-20 09:00:56 -0400

    Reformulated potential energy difference; got same results

---
+++ build/pigsfli/CMakeFiles/Export/share/pigsfli.e/cmake/pigsfli.eConfig-none.cmake
@@ -0,0 +1,18 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "None".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pigsfli.e" for configuration "None"
+set_property(TARGET pigsfli.e APPEND PROPERTY IMPORTED_CONFIGURATIONS NONE)
+set_target_properties(pigsfli.e PROPERTIES
+  IMPORTED_LOCATION_NONE "${_IMPORT_PREFIX}/bin/pigsfli.e"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS pigsfli.e )
+list(APPEND _IMPORT_CHECK_FILES_FOR_pigsfli.e "${_IMPORT_PREFIX}/bin/pigsfli.e" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
